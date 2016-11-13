fabb6e835a2227cef17bf3960d4f0dc13d2964d4
splogamurugan1 <sploga.murugan@gmail.com>
2016-11-13 21:43:09 +0530

    added processScreenshot

--- job.js
+++ job.js
@@ -2,6 +2,7 @@ var Jobs = require('./jobs');
 var utils = require('./utils');
 
 var job    = process.argv[2] || 'processUrls'; 
+var job    = job.trim();
 var params = {status:process.argv[3] || 0}; 
 
 if (job == 'processUrls') {
@@ -15,6 +16,10 @@ if (job == 'processUrls') {
 } else if(job == 'transformUrls') {
 	var params = {status:process.argv[3] || 1}; 
 	j[job](process.argv[3]);	
+} else if(job == 'processScreenShots') {
+	var j = new Jobs({});
+	var params = {status:process.argv[3] || 2}; 
+	j[job](params);	
 } else {
 	var j = new Jobs({});
 	j[job](process.argv[3], process.argv[4]);
