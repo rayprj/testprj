7f07dae022e7722eaa1cde235cc12e7613fde66b
rayprj <sploga.murugan@gmail.com>
2016-12-07 23:29:33 +0530

    Added rating related files

--- post-process.js
+++ post-process.js
@@ -5,6 +5,8 @@ const Promise = require('promise');
 const db      = require('./database/manager');
 const config  = require('./config/config.js');
 const utils = require('./utils');
+var Q  = require('q');
+
 var proxyEnv = process.env.PROXY_ENV || '';
 function requestCallBack(urlId,error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -39,11 +41,18 @@ function sendRequest(url,requestCallBack){
 }
 
 
-function initPostScripts(){
+function initPostScripts(limit){
     "use strict";
+    
+    
     utils.db.getPostProccessConfigs().then(function(processConfigs){
-        utils.db.getUrlContents('0').then(function(urlData){
-            
+        utils.db.getUrlContents('0', limit).then(function(urlData){
+
+            if (utils._.isEmpty(urlData)) {
+                console.log('No data to process ');
+                process.exit();
+            }
+            var promises = [];
             for(let i=0;i<urlData.length;i++){
                 let urlRow = urlData[i];
                 let domain = urlRow.domain.replace(/\./g,"");
@@ -54,13 +63,15 @@ function initPostScripts(){
                         let html = urlRow.content;
                         //console.log(domain,type,url)
                         if(processors[domain] && processors[domain][type]){
-
+                            let deferred = Q.defer()
+                            promises.push(deferred.promise)
                             processors[domain][type]['preProcessData'](html,url).then(function(serviceUrl){
                                 
                                 sendRequest(serviceUrl,function(error, response, body){
                                     if (!error && response.statusCode == 200) {
                                         processors[domain][type]['processData'](body).then(function(data){
-                                            processors[domain][type]['postProcessData'](data,urlRow);
+                                            processors[domain][type]['postProcessData'](data,urlRow,deferred);
+                                            //console.log(promises);
                                         });
                                     }
                                     else{
@@ -73,9 +84,20 @@ function initPostScripts(){
                 }
                  
             }
-            
-        })
+            //console.log(promises);
+            Q.allSettled(promises).then(function(r){
+                //console.log('Processed all the post process');
+                //process.exit();
+                initPostScripts(limit);
+            }).catch(function(e){
+                console.log('Couldnt process all the post process');
+                console.log(e);
+                process.exit();
+            })
+                
+            })
     })
+    //return promises;
 }
 
-initPostScripts();
+module.exports = initPostScripts;
